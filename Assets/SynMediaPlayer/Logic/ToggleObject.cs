f06c530775e3e775449edb43c27e70c146830bff
Synergiance <syn@synerfiles.info>
2021-10-20 18:13:28 -0400

    Make info button work (#2)
    - Add object toggler behaviour
    - Unpack info button prefab
    - Disable info panel by default

---
+++ Assets/SynMediaPlayer/Logic/ToggleObject.cs
@@ -0,0 +1,17 @@
+﻿
+using Synergiance.MediaPlayer.UI;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Synergiance.MediaPlayer.Helpers {
+	public class ToggleObject : UdonSharpBehaviour {
+		public GameObject toggleObject;
+
+		public void _Toggle() {
+			if (toggleObject == null) return;
+			toggleObject.SetActive(!toggleObject.activeSelf);
+		}
+	}
+}
