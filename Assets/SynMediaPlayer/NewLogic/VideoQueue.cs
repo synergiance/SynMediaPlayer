91627fab5727e9704990f6060f65b23a4cea97ab
Synergiance <syn@synerfiles.info>
2022-08-23 23:12:24 -0400

    Initial video queue

---
+++ Assets/SynMediaPlayer/NewLogic/VideoQueue.cs
@@ -0,0 +1,66 @@
+﻿using Synergiance.MediaPlayer.Diagnostics;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Synergiance.MediaPlayer {
+	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class VideoQueue : DiagnosticBehaviour {
+		[SerializeField] private int maxQueueLength = 64;
+		[UdonSynced] private VRCUrl[] queuedVideos; // This will be a ring buffer
+		[UdonSynced] private int currentIndex;
+		[UdonSynced] private int videosInQueue;
+
+		private bool initialized;
+
+		public VRCUrl CurrentVideo => GetVideoAtIndex(0);
+		public VRCUrl NextVideo => GetVideoAtIndex(1);
+		public int VideosInQueue => videosInQueue;
+
+		protected override string DebugName => "Video Queue";
+		protected override string DebugColor => "#A08040";
+
+		private void Start() {
+			Initialize();
+		}
+
+		private void Initialize() {
+			if (initialized) return;
+			if (maxQueueLength < 4) {
+				Debug.LogWarning("Max queue length (" + maxQueueLength + ") is too low! Increasing to 4.");
+				maxQueueLength = 4;
+			}
+			Log("Initializing ring buffer of length " + maxQueueLength);
+			queuedVideos = new VRCUrl[maxQueueLength];
+			initialized = true;
+		}
+
+		private int CalcRingIndex(int _index) {
+			return (_index + currentIndex) % maxQueueLength;
+		}
+
+		public VRCUrl GetVideoAtIndex(int _index) {
+			Initialize();
+			return queuedVideos[CalcRingIndex(_index)];
+		}
+
+		public void AdvanceQueue() {
+			if (videosInQueue <= 0) {
+				LogError("Queue empty, cannot advance queue!");
+				return;
+			}
+			currentIndex = (currentIndex + 1) % maxQueueLength;
+			videosInQueue = Mathf.Max(videosInQueue - 1, 0);
+		}
+
+		public bool AddVideo(VRCUrl _link) {
+			if (videosInQueue >= maxQueueLength) {
+				LogError("Cannot insert into queue, max queue length reached!");
+				return false;
+			}
+			Log("Adding to queue: " + _link);
+			queuedVideos[CalcRingIndex(videosInQueue++)] = _link;
+			return true;
+		}
+	}
+}
