9e096c58522d194eb5a6927be241fafcf861a71b
Synergiance <syn@synerfiles.info>
2024-01-04 21:49:23 -0500

    Version 1.1.0
    - Finalize new version
    - Remove old prefabs
    - Correct initial interpolator material values

--- Assets/SynMediaPlayer/Logic/MediaPlayer.cs
+++ Assets/SynMediaPlayer/Logic/MediaPlayer.cs
@@ -181,7 +181,7 @@ namespace Synergiance.MediaPlayer {
 		private ushort localVersionMajor =  1; // Major version number
 		private ushort localVersionMinor =  1; // Minor version number
 		private ushort localVersionPatch =  0; // Patch version number
-		private ushort localVersionBeta  =  3; // Beta number
+		private ushort localVersionBeta  =  0; // Beta number
 
 		private ushort worldVersionMajor; // Major version number
 		private ushort worldVersionMinor; // Minor version number
