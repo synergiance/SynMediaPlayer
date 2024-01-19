fdaa3b4e99ee12d1ee5a6f978fbdbff532a7d4c9
Synergiance <syn@synerfiles.info>
2024-01-19 02:17:23 -0500

    Version 1.2.0

--- Assets/SynMediaPlayer/Logic/MediaPlayer.cs
+++ Assets/SynMediaPlayer/Logic/MediaPlayer.cs
@@ -189,7 +189,7 @@ namespace Synergiance.MediaPlayer {
 		private ushort localVersionMajor =  1; // Major version number
 		private ushort localVersionMinor =  2; // Minor version number
 		private ushort localVersionPatch =  0; // Patch version number
-		private ushort localVersionBeta  =  2; // Beta number
+		private ushort localVersionBeta  =  0; // Beta number
 
 		private ushort worldVersionMajor; // Major version number
 		private ushort worldVersionMinor; // Minor version number
